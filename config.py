a55ecd11d85ec99b47ae14beb49c3950b9367d20
Craighton Hancock <craightonh@byu.edu>
2022-08-02 23:14:38 -0600

    add login and logout

---
+++ config.py
@@ -0,0 +1,10 @@
+driver_opts = ["--headless"]
+login_url = "https://www.apsystemsema.com/ema/index.action"
+username_element_id = "username"
+password_element_id = "password"
+login_button_element_id = "Login"
+sign_out_text = "Sign out"
+exception_page = "ema/exceptionIndex.action"
+
+api_getDashboardProductionInfo = "https://www.apsystemsema.com/ema/ajax/getDashboardApiAjax/getDashboardEcuPowerOnCurrentDayAjax"
+
--- main.py
+++ main.py
@@ -1,21 +1,62 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.firefox.options import Options
+import config
+import secret
+from time import sleep
 
-# conf = 
+def setupDriver():
+    options = Options()
+    for opt in config.driver_opts: 
+        options.add_argument(opt)
+    return webdriver.Firefox(options=options)
 
-options = Options()
-options.add_argument("--headless")
-cap = DesiredCapabilities().FIREFOX
-cap["marionette"] = False
-driver = webdriver.Firefox(capabilities=cap, options=options, )
-driver.get("https://www.apsystemsema.com/ema/index.action")
-assert "Python" in driver.title
-elem = driver.find_element(By.NAME, "q")
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
-driver.close()
+def logout(driver):
+    top_menu = driver.find_element(By.CLASS_NAME, "div_menu2")
+    anchors = top_menu.find_elements(By.TAG_NAME, "a")
+    sign_out = anchors[len(anchors) - 1]
+    assert sign_out.text == config.sign_out_text
+    sign_out.click()
+    print(driver.current_url)
+    driver.close()
+
+def login(driver):
+    try:
+        driver.get(config.login_url)
+        # print("current url: " + driver.current_url)
+
+        input_username = driver.find_element(By.ID, config.username_element_id)
+        input_username.clear()
+        input_username.send_keys(secret.username)
+        # print("input.username: " + input_username.get_attribute("value"))
+
+        input_password = driver.find_element(By.ID, config.password_element_id)
+        input_password.clear()
+        input_password.send_keys(secret.password)
+        # print("input.password: " + input_password.get_attribute("value"))
+
+        assert input_username.get_attribute("value") == secret.username and input_password.get_attribute("value") == secret.password
+
+        driver.find_element(By.ID, config.login_button_element_id).click()
+
+        assert config.exception_page not in driver.current_url
+
+        return True
+    except:
+        print("Username field not found.")
+        return False
+
+def getProductionInfo(driver):
+    return False
+
+if __name__ == "__main__":
+    driver = setupDriver()
+    print("Logging in to " + config.login_url)
+    if login(driver):
+        # print("cookies: " + driver.get_cookies)
+        print("Successfully logged in")
+        getProductionInfo(driver)
+        sleep(2)
+        logout(driver)
+    else:
+        print("ERROR: Unable to login")

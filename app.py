7164137204f83a5ca61b1fedd7b2947d31ad375e
Craighton Hancock <hancockcraighton@gmail.com>
2022-08-10 21:55:52 -0600

    fix linter issues

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
+"""
+Logs into EMA web app and grabs metrics from EMA's API
+"""
+from time import sleep
+import traceback
+import os
+import logging
+from sys import exit as sys_exit
+import yaml
+from paho.mqtt import publish
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
 from requests import post
-from time import sleep
-from sys import exit as sys_exit
-import paho.mqtt.publish as publish, traceback, os, yaml, logging
+
 config = {}
 secret = {}
 log = logging.getLogger("app.py")
-def appSetup():
+
+def app_setup():
+    """
+    Sets up config, secrets, and logging
+    """
     log_level_opt = {
         "debug": logging.DEBUG,
         "info": logging.INFO,
@@ -18,12 +30,14 @@ def appSetup():
         "critical": logging.CRITICAL
     }
     try:
-        config_dir = os.getenv('CONFIG_DIRECTORY_NAME') if os.getenv('CONFIG_DIRECTORY_NAME') is not None else 'config'
+        config_dir = 'config'
+        if os.environ["CONFIG_DIRECTORY_NAME"] is not None:
+            config_dir = os.environ["CONFIG_DIRECTORY_NAME"]
         for file in os.listdir(config_dir):
             if os.path.isfile(os.path.join(config_dir, file)):
-                with open(f'{config_dir}/{file}', 'r') as config_stream:
+                with open(f'{config_dir}/{file}', 'r', encoding="utf-8") as config_stream:
                     config.update(yaml.safe_load(config_stream))
-    except FileNotFoundError as err:
+    except FileNotFoundError:
         print("Could not find config file. Please review documentation on config file location/format.")
         sys_exit(1)
 
@@ -34,10 +48,12 @@ def appSetup():
     log.addHandler(log_handle)
 
     try:
-        secrets_dir = os.getenv('SECRETS_DIRECTORY_NAME') if os.getenv('SECRETS_DIRECTORY_NAME') is not None else 'secrets'
+        secrets_dir = 'secrets'
+        if os.environ["SECRETS_DIRECTORY_NAME"] is not None:
+            secrets_dir = os.environ["SECRETS_DIRECTORY_NAME"]
         for file in os.listdir(secrets_dir):
             if os.path.isfile(os.path.join(secrets_dir, file)):
-                with open(f'{secrets_dir}/{file}', 'r') as secret_stream:
+                with open(f'{secrets_dir}/{file}', 'r', encoding="utf-8") as secret_stream:
                     secret.update(yaml.safe_load(secret_stream))
     except FileNotFoundError:
         try:
@@ -47,17 +63,23 @@ def appSetup():
             secret["mqtt"]["hostname"] = os.environ["MQTT_HOSTNAME"]
             secret["mqtt"]["port"] = os.environ["MQTT_PORT"]
         except KeyError as err:
-            log.error("Environment variable not found: " + str(err) + ". Quitting with non-zero exit code.")
+            log.error("Environment variable not found: %s. Quitting with non-zero exit code.", err)
             sys_exit(1)
 
-def setupDriver():
+def setup_driver():
+    """
+    Sets up the Firefox driver from config
+    """
     options = Options()
     for opt in config["webdriver"]["driver_opts"]:
         options.add_argument(opt)
     return webdriver.Firefox(options=options)
 
 def logout(driver: webdriver.Firefox):
-    log.debug("(logout) driver.current_url: " + driver.current_url)
+    """
+    Logs out of EMA web app
+    """
+    log.debug("(logout) driver.current_url: %s", driver.current_url)
     sleep(1)
     top_menu = driver.find_element(By.CLASS_NAME, "div_menu2")
     anchors = top_menu.find_elements(By.TAG_NAME, "a")
@@ -69,27 +91,33 @@ def logout(driver: webdriver.Firefox):
     return True
 
 def login(driver: webdriver.Firefox):
-    log.info("(login) Logging in to " + config["auth"]["login_url"])
+    """
+    Logs in to EMA web app
+    """
+    log.info("(login) Logging in to %s", config["auth"]["login_url"])
     success = False
     driver.get(config["auth"]["login_url"])
-    log.debug("(login) driver.current_url: " + driver.current_url)
+    log.debug("(login) driver.current_url: %s", driver.current_url)
     input_username = driver.find_element(By.ID, config["auth"]["username_element_id"])
     input_username.clear()
     input_username.send_keys(secret["auth"]["username"])
-    log.debug("(login) input.username: " + input_username.get_attribute("value"))
+    log.debug("(login) input.username: %s", input_username.get_attribute("value"))
     input_password = driver.find_element(By.ID, config["auth"]["password_element_id"])
     input_password.clear()
     input_password.send_keys(secret["auth"]["password"])
     if config["logging"]["redact_sensitive_fields"]:
         log.debug("(login) input.password: [REDACTED]")
     else:
-        log.debug("(login) input.password: " + input_password.get_attribute("value"))
+        log.debug("(login) input.password: %s", input_password.get_attribute("value"))
     driver.find_element(By.ID, config["auth"]["login_button_element_id"]).click()
     success = config["auth"]["exception_page"] not in driver.current_url
     return success
 
-def getProductionInfo(cookies):
-    log.info("(getProductionInfo) Acquiring production info")
+def get_production_info(cookies):
+    """
+    Requests data from EMA getProductionInfo API endpoint
+    """
+    log.info("(get_production_info) Acquiring production info")
     headers = config["api"]["headers"]
     endpoint = config["api"]["endpoints"]["getProductionInfo"]
     cookie_dict = {}
@@ -98,42 +126,55 @@ def getProductionInfo(cookies):
     response = post(url=endpoint, cookies=cookie_dict, headers=headers)
     if not response.ok:
         raise Exception("Error retrieving production info: " + response.reason)
-    log.info("(getProductionInfo) Successfully acquired production info")
+    log.info("(get_production_info) Successfully acquired production info")
     return response.json()
 
-def publishProductionInfo(data):
-    log.info("(publishProductionInfo) Publishing production info to mqtt broker at " + secret["mqtt"]["hostname"] + ":" + str(secret["mqtt"]["port"]))
-    publish.single(topic=config["mqtt"]["topic"]["prefix"] + config["mqtt"]["topic"]["current_power"], payload=data[config["response_fields"][config["mqtt"]["topic"]["current_power"]]], qos=config["mqtt"]["qos"], hostname=secret["mqtt"]["hostname"], port=int(secret["mqtt"]["port"]), client_id=config["mqtt"]["client_id"], retain=bool(config["mqtt"]["retainStatistics"]))
-    publish.single(topic=config["mqtt"]["topic"]["prefix"] + config["mqtt"]["topic"]["energy_today"], payload=data[config["response_fields"][config["mqtt"]["topic"]["energy_today"]]], qos=config["mqtt"]["qos"], hostname=secret["mqtt"]["hostname"], port=int(secret["mqtt"]["port"]), client_id=config["mqtt"]["client_id"], retain=bool(config["mqtt"]["retainStatistics"]))
-    publish.single(topic=config["mqtt"]["topic"]["prefix"] + config["mqtt"]["topic"]["energy_lifetime"], payload=data[config["response_fields"][config["mqtt"]["topic"]["energy_lifetime"]]], qos=config["mqtt"]["qos"], hostname=secret["mqtt"]["hostname"], port=int(secret["mqtt"]["port"]), client_id=config["mqtt"]["client_id"], retain=bool(config["mqtt"]["retainStatistics"]))
-    publish.single(topic=config["mqtt"]["topic"]["prefix"] + config["mqtt"]["topic"]["co2_saved"], payload=data[config["response_fields"][config["mqtt"]["topic"]["co2_saved"]]], qos=config["mqtt"]["qos"], hostname=secret["mqtt"]["hostname"], port=int(secret["mqtt"]["port"]), client_id=config["mqtt"]["client_id"], retain=bool(config["mqtt"]["retainStatistics"]))
-    publish.single(topic=config["mqtt"]["topic"]["prefix"] + config["mqtt"]["topic"]["monitor_status"], payload=data[config["response_fields"][config["mqtt"]["topic"]["monitor_status"]]], qos=config["mqtt"]["qos"], hostname=secret["mqtt"]["hostname"], port=int(secret["mqtt"]["port"]), client_id=config["mqtt"]["client_id"], retain=bool(config["mqtt"]["retainMonitorStatus"]))
+def publish_production_info(data):
+    """
+    Send data to MQTT broker
+    """
+    hostname = secret["mqtt"]["hostname"]
+    port = int(secret["mqtt"]["port"])
+    # pylint: disable=C0301
+    log.info("(publish_production_info) Publishing production info to mqtt broker at %s:%s", hostname, port)
+    topic_prefix = config["mqtt"]["topic_prefix"]
+    client_id = config["mqtt"]["client_id"]
+    for topic in config["mqtt"]["topics"]:
+        name = topic_prefix + topic["name"]
+        qos = topic["qos"]
+        retain = bool(topic["retain"])
+        payload = data[config["response_fields"][topic["name"]]]
+        publish.single(topic=name, payload=payload, qos=qos, retain=retain, hostname=hostname, port=port, client_id=client_id)
 
 if __name__ == "__main__":
-    appSetup()
-    driver = setupDriver()
+    app_setup()
+    web_driver = setup_driver()
     interval = config["exception_handling"]["initial_interval"]
     max_attempts = config["exception_handling"]["max_attempts"]
+    # pylint: disable=C0103
     num_attempts = 0
-    while(num_attempts != max_attempts):
+    while num_attempts != max_attempts:
         num_attempts += 1
         try:
-            if login(driver):
+            if login(web_driver):
                 log.info("(__main__) Successfully logged in")
-                json_data = getProductionInfo(driver.get_cookies())
-                publishProductionInfo(json_data)
-                if logout(driver):
+                json_data = get_production_info(web_driver.get_cookies())
+                publish_production_info(json_data)
+                if logout(web_driver):
                     log.info("(__main__) Successfully retrieved data. Exiting with success code 0.")
                     sys_exit(0)
                 else:
+                    # pylint: disable=C0301
                     log.warning("(__main__) Failed to logout properly. Data was collected, but logout issues should be investigated. Exiting with success code 0.")
                     sys_exit(0)
             else:
                 raise Exception("Failed login")
+        # pylint: disable=W0703
         except Exception as ex:
             trace = traceback.format_exc()
-            log.error("(__main__) " + str(type(ex)) + " " + str(ex) + " " + str(trace))
-        log.info("(__main__) Initiating backoff. Attempts remaining: " + str(max_attempts - num_attempts) + ". Will retry in " + str(interval) + " seconds.")
+            log.error("(__main__) %s %s %s", type(ex), ex, trace)
+        # pylint: disable=C0301
+        log.info("(__main__) Initiating backoff. Attempts remaining: %i. Will retry in %i seconds.", (max_attempts - num_attempts), interval)
         if num_attempts > 0:
             interval = interval * config["exception_handling"]["backoff_multiplier"]
         sleep(interval)
